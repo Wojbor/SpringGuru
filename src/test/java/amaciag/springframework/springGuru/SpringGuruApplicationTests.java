e4db592d903b008937a706b797b5269cc9a64a7e
Wojbor <arkadiusz.maciag1@gmail.com>
2024-08-09 22:52:51 +0200

    repo init

---
+++ src/test/java/amaciag/springframework/springGuru/SpringGuruApplicationTests.java
@@ -0,0 +1,13 @@
+package amaciag.springframework.springGuru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringGuruApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
