7e5bc5880c40be7a29e38f34af35e379b49cc02b
Wojbor <arkadiusz.maciag1@gmail.com>
2024-08-12 22:01:29 +0200

    controller context

--- src/main/java/amaciag/springframework/springGuru/SpringGuruApplication.java
+++ src/main/java/amaciag/springframework/springGuru/SpringGuruApplication.java
@@ -1,13 +1,22 @@
 package amaciag.springframework.springGuru;
 
+import amaciag.springframework.springGuru.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringGuruApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringGuruApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SpringGuruApplication.class, args);
+
+		MyController myController = context.getBean(MyController.class);
+
+		System.out.println("I'm in the Main");
+
+
+		System.out.println(myController.sayHello());
 	}
 
 }
---
+++ src/main/java/amaciag/springframework/springGuru/controllers/MyController.java
@@ -0,0 +1,14 @@
+package amaciag.springframework.springGuru.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello() {
+        System.out.println("I'm in the controller");
+
+        return "Hello World from controller";
+    }
+
+}
